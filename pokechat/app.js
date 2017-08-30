4f8d0b3937cd4beebd7e29e12f88d8350709ceb6
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2017-08-29 22:22:37 -0500

    add events with socket.io

--- pokechat/app.js
+++ pokechat/app.js
@@ -5,11 +5,5 @@
 import React from "react";
 import PokeApp from "./components/PokeApp";
 
-const pokemons = [
-  { number: 1, name: "Bulbasaur" },
-  { number: 2, name: "Ivysaur" },
-  { number: 3, name: "Venusaur" }
-];
-
 const app = document.getElementById("container");
-React.render(<PokeApp pokemons={pokemons} />, app);
+React.render(<PokeApp />, app);
--- pokechat/index.js
+++ pokechat/index.js
@@ -5,6 +5,7 @@
 import express from "express";
 import http from "http";
 import engine from "socket.io";
+import dbapi from "./db-api";
 
 const port = 3000;
 const app = express();
@@ -12,10 +13,24 @@ const app = express();
 // Configurar la ruta de archivos estáticos
 app.use("/", express.static(__dirname + "/public"));
 
+app.get("/pokemons", (req, res) => {
+  dbapi.pokemons.find(pokemons => {
+    res.json(pokemons);
+  });
+});
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-http.createServer(app).listen(port, () => {
+const server = http.createServer(app).listen(port, () => {
   console.log(`El servidor está escuchando en el puerto ${port}`);
+});
+
+const io = engine.listen(server);
+
+io.on("connection", socket => {
+  socket.on("message", msg => {
+    io.emit("message", msg);
+  });
 });
